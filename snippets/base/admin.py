b9aac5c14783733b5224a37b9f67b53c86d6c63c
Michael Kelly <mkelly@mozilla.com>
2013-05-23 17:34:05 -0400

    Fix Bug 870486: Add snippet templates.
    Adds a model SnippetTemplate, which contains a jinja2 template that is
    used to render a snippet. Info about the variables available in the
    template is stored as SnippetTemplateVariables.
    
    The Snippet admin now presents a widget that lets admins choose which
    template to use, and shows them fields for filling in the variables
    from the template. When rendered, the snippets render the template,
    passing in the data entered in the admin to generate the output.

--- snippets/base/admin.py
+++ snippets/base/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from django.db.models import TextField, Q
 
-from snippets.base import models
+from django_ace import AceWidget
+from jingo import env, load_helpers
+from jinja2.meta import find_undeclared_variables
+
+from snippets.base import forms, models
 
 
 class SnippetAdmin(admin.ModelAdmin):
+    form = forms.SnippetAdminForm
+
     list_display = ('name', 'priority', 'disabled', 'publish_start',
                     'publish_end', 'created', 'modified')
     list_filter = ('disabled', 'client_match_rules')
@@ -12,8 +19,11 @@ class SnippetAdmin(admin.ModelAdmin):
     filter_horizontal = ('client_match_rules',)
 
     fieldsets = (
-        (None, {'fields': ('name', 'body', 'priority', 'disabled',
+        (None, {'fields': ('name', 'priority', 'disabled',
                            'created', 'modified')}),
+        ('Content', {
+            'fields': ('template', 'data'),
+        }),
         ('Publish Duration', {
             'description': 'When will this snippet be available? (Optional)',
             'fields': ('publish_start', 'publish_end'),
@@ -28,3 +38,51 @@ admin.site.register(models.Snippet, SnippetAdmin)
 class ClientMatchRuleAdmin(admin.ModelAdmin):
     pass
 admin.site.register(models.ClientMatchRule, ClientMatchRuleAdmin)
+
+
+class SnippetTemplateVariableInline(admin.TabularInline):
+    model = models.SnippetTemplateVariable
+    max_num = 0
+    can_delete = False
+    readonly_fields = ('name',)
+    fields = ('name', 'type',)
+
+
+RESERVED_VARIABLES = ('_',)
+
+
+class SnippetTemplateAdmin(admin.ModelAdmin):
+    inlines = (SnippetTemplateVariableInline,)
+    formfield_overrides = {
+        TextField: {'widget': AceWidget(mode='html', theme='github', attrs={'cols': 500})},
+    }
+
+    class Media:
+        css = {
+            'all': ('css/admin.css',)
+        }
+
+    def save_related(self, request, form, formsets, change):
+        """
+        After saving the related objects, remove and add
+        SnippetTemplateVariables depending on how the template code changed.
+        """
+        super(SnippetTemplateAdmin, self).save_related(request, form, formsets,
+                                                       change)
+        load_helpers()  # Ensure jingo helpers are loaded.
+
+        # Parse the template code and find any undefined variables.
+        ast = env.parse(form.instance.code)
+        new_vars = find_undeclared_variables(ast)
+        var_manager = form.instance.variable_set
+
+        # Filter out reserved variable names.
+        new_vars = filter(lambda x: not x in RESERVED_VARIABLES, new_vars)
+
+        # Delete variables not in the new set.
+        var_manager.filter(~Q(name__in=new_vars)).delete()
+
+        # Create variables that don't exist.
+        for variable in new_vars:
+            var_manager.get_or_create(name=variable)
+admin.site.register(models.SnippetTemplate, SnippetTemplateAdmin)
---
+++ snippets/base/forms.py
@@ -0,0 +1,86 @@
+import json
+
+from collections import defaultdict
+
+from django import forms
+from django.conf import settings
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+
+from snippets.base.models import Snippet, SnippetTemplateVariable
+
+
+class TemplateSelect(forms.Select):
+    """
+    Select box used for choosing a template. Adds extra metadata to each
+    <option> element detailing the template variables available in the
+    corresponding template.
+    """
+    def render(self, *args, **kwargs):
+        # Retrieve data about the currently available template variables from
+        # the database prior to rendering the options.
+        self.variables_for = defaultdict(list)
+        for variable in SnippetTemplateVariable.objects.all():
+            self.variables_for[variable.template.id].append({
+                'name': variable.name,
+                'type': variable.type,
+            })
+
+        return super(TemplateSelect, self).render(*args, **kwargs)
+
+    def render_option(self, selected_choices, option_value, option_label):
+        output = super(TemplateSelect, self).render_option(
+            selected_choices, option_value, option_label)
+
+        # Attach a list of template variables for the template this option
+        # represents as a data attribute.
+        try:
+            attr_value = json.dumps(self.variables_for[int(option_value)])
+            attr = 'data-variables="{0}"'.format(escape(attr_value))
+            output = output.replace('<option', '<option {0}'.format(attr))
+        except ValueError:
+            pass  # Value wasn't an int, no need for an attribute.
+
+        return mark_safe(output)
+
+
+class TemplateDataWidget(forms.TextInput):
+    """
+    A widget that links up with a TemplateSelect and shows fields for each of
+    the template variables available for the currently selected template. These
+    fields are combined into a JSON string when the form is submitted.
+    """
+    def __init__(self, select_name, *args, **kwargs):
+        self.template_select_name = select_name
+        return super(TemplateDataWidget, self).__init__(*args, **kwargs)
+
+    def render(self, name, value, attrs=None):
+        widget_code = super(TemplateDataWidget, self).render(
+            name, value, attrs)
+        return mark_safe(u''.join([widget_code, u"""
+            <div class="template-data-widget"
+                 data-select-name="{0}"
+                 data-input-name="{1}">
+            </div>
+        """.format(self.template_select_name, name)]))
+
+    class Media:
+        css = {
+            'all': ('css/templateDataWidget.css',)
+        }
+        js = filter(None, [
+            'js/lib/jquery-2.0.0.min.js',
+            'js/lib/nunjucks-{0}.js'.format(
+                'dev' if settings.DEBUG else 'min'),
+            'templates/compiled.js' if not settings.DEBUG else None,
+            'js/templateDataWidget.js'
+        ])
+
+
+class SnippetAdminForm(forms.ModelForm):
+    class Meta:
+        model = Snippet
+        widgets = {
+            'template': TemplateSelect,
+            'data': TemplateDataWidget('template'),
+        }
