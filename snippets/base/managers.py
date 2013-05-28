1338c8f4f06bb005c3130e78be9f43c0e09db064
Giorgos Logiotatidis <seadog@sealabs.net>
2013-05-28 16:18:49 +0300

    [fix bug 870495] Common match rules with checkboxes.

--- snippets/base/managers.py
+++ snippets/base/managers.py
@@ -16,3 +16,29 @@ class ClientMatchRuleQuerySet(QuerySet):
 class ClientMatchRuleManager(Manager):
     def get_query_set(self):
         return ClientMatchRuleQuerySet(self.model)
+
+
+class SnippetManager(Manager):
+    def match_client(self, client):
+        from snippets.base.models import (CHANNELS, CLIENT_NAMES,
+                                          STARTPAGE_VERSIONS)
+        filters = {}
+        # Retrieve the first item that starts with an available item. Allows
+        # things like "release-cck-mozilla14" to match "release".
+        client_channel = next((channel for channel in CHANNELS if
+                               client.channel.startswith(channel)), False)
+        if client_channel:
+            filters.update(**{'on_{0}'.format(client_channel):True})
+
+        client_startpage_version = next(
+            (version for version in STARTPAGE_VERSIONS if
+             client.startpage_version.startswith(version)), False)
+        if client_startpage_version:
+            filters.update(
+                **{'on_startpage_{0}'.format(client_startpage_version):True})
+
+        client_name = CLIENT_NAMES.get(client.name, False)
+        if client_name:
+            filters.update(**{'on_{0}'.format(client_name):True})
+
+        return self.filter(**filters)
