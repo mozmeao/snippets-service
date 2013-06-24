01e064c3e41c803f39c894bc5abbf2f1e547ce40
Giorgos Logiotatidis <seadog@sealabs.net>
2013-06-24 13:27:08 +0300

    Tests for public index page.

--- snippets/base/tests/test_views.py
+++ snippets/base/tests/test_views.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
+from funfactory.helpers import urlparams
+
 from mock import patch
 from nose.tools import eq_
 
@@ -167,3 +169,62 @@ class PreviewSnippetTests(TestCase):
         snippet = response.context['snippet']
         eq_(snippet.template, template)
         eq_(snippet.data, data)
+
+
+@patch('snippets.base.views.SNIPPETS_PER_PAGE', 1)
+class IndexSnippetsTests(TestCase):
+    def setUp(self):
+        for i in range(10):
+            SnippetFactory.create()
+
+    def test_base(self):
+        response = self.client.get(reverse('base.index'))
+        eq_(response.status_code, 200)
+        eq_(response.context['snippets'].number, 1)
+
+    def test_second_page(self):
+        response = self.client.get(urlparams(reverse('base.index'), page=2))
+        eq_(response.status_code, 200)
+        eq_(response.context['snippets'].number, 2)
+        eq_(response.context['snippets'].paginator.num_pages, 10)
+
+    def test_empty_page_number(self):
+        """Test that empty page number returns the last page."""
+        response = self.client.get(urlparams(reverse('base.index'), page=20))
+        eq_(response.status_code, 200)
+        eq_(response.context['snippets'].number, 10)
+        eq_(response.context['snippets'].paginator.num_pages, 10)
+
+    def test_non_integer_page_number(self):
+        """Test that a non integer page number returns the first page."""
+        response = self.client.get(urlparams(reverse('base.index'), page='k'))
+        eq_(response.status_code, 200)
+        eq_(response.context['snippets'].number, 1)
+        eq_(response.context['snippets'].paginator.num_pages, 10)
+
+    def test_filter(self):
+        SnippetFactory.create(on_nightly=True)
+        response = self.client.get(urlparams(reverse('base.index'), on_nightly=2))
+        eq_(response.status_code, 200)
+        eq_(response.context['snippets'].paginator.count, 1)
+
+    def test_pagination_range_first_page(self):
+        response = self.client.get(reverse('base.index'))
+        pagination_range = response.context['pagination_range']
+        eq_(pagination_range[0], 1)
+        eq_(pagination_range[-1], 3)
+        eq_(len(pagination_range), 3)
+
+    def test_pagination_range_last_page(self):
+        response = self.client.get(urlparams(reverse('base.index'), page=10))
+        pagination_range = response.context['pagination_range']
+        eq_(pagination_range[0], 8)
+        eq_(pagination_range[-1], 10)
+        eq_(len(pagination_range), 3)
+
+    def test_pagination_range_middle_page(self):
+        response = self.client.get(urlparams(reverse('base.index'), page=5))
+        pagination_range = response.context['pagination_range']
+        eq_(pagination_range[0], 3)
+        eq_(pagination_range[-1], 7)
+        eq_(len(pagination_range), 5)
