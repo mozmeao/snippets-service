6fa89c508d308d5245fa7ac8b28c0a8981d2d5f7
Michael Kelly <mkelly@mozilla.com>
2013-11-14 16:17:59 -0500

    Revert ec92c0ce: We don't need to block out New Relic anymore.

--- snippets/base/middleware.py
+++ snippets/base/middleware.py
@@ -13,20 +13,8 @@ class FetchSnippetsMiddleware(object):
     middlewares. To avoid unintended issues (such as headers we don't want
     being added to the response) this middleware detects requests to that view
     and executes the view early, bypassing the rest of the middleware.
-
-    Also disables New Relic's apdex for views that aren't the
-    fetch_snippets, as we only really care about the apdex for
-    fetch_snippets.
     """
     def process_request(self, request):
         result = resolve(request.path)
         if result.func == fetch_snippets:
             return fetch_snippets(request, *result.args, **result.kwargs)
-        else:
-            # Not fetch_snippets? Then no New Relic for you!
-            try:
-                import newrelic.agent
-            except ImportError:
-                pass
-            else:
-                newrelic.agent.suppress_apdex_metric()
