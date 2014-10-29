c1b21bf38be78df5d9299fc4e4af9438877322fc
Giorgos Logiotatidis <seadog@sealabs.net>
2014-10-29 14:53:11 +0200

    Add travis.

--- snippets/settings/__init__.py
+++ snippets/settings/__init__.py
@@ -2,11 +2,16 @@ import sys
 
 
 from .base import *
-try:
-    from .local import *
-except ImportError, exc:
-    exc.args = tuple(['%s (did you rename settings/local.py-dist?)' % exc.args[0]])
-    raise exc
+
+# Load special settings for CI.
+if os.environ.get('TRAVIS'):
+    from .travis import *
+else:
+    try:
+        from .local import *
+    except ImportError, exc:
+        exc.args = tuple(['%s (did you rename settings/local.py-dist?)' % exc.args[0]])
+        raise exc
 
 TEST = len(sys.argv) > 1 and sys.argv[1] == 'test'
 if TEST:
---
+++ snippets/settings/travis.py
@@ -0,0 +1,42 @@
+from . import base
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'snippets',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': 'SET storage_engine=InnoDB',
+            'charset' : 'utf8',
+            'use_unicode' : True,
+        },
+        'TEST_CHARSET': 'utf8',
+        'TEST_COLLATION': 'utf8_general_ci',
+    },
+}
+
+DEBUG = TEMPLATE_DEBUG = False
+
+HMAC_KEYS = {
+    '2012-06-06': 'some secret',
+}
+
+from django_sha2 import get_password_hashers
+PASSWORD_HASHERS = get_password_hashers(base.BASE_PASSWORD_HASHERS, HMAC_KEYS)
+
+# Make this unique, and don't share it with anybody.  It cannot be blank.
+SECRET_KEY = 'dev'
+
+# Should robots.txt allow web crawlers?  Set this to True for production
+ENGAGE_ROBOTS = True
+
+SESSION_COOKIE_SECURE = False
+
+# Snippets-specific caching
+SNIPPET_HTTP_MAX_AGE = 90  # Time to cache HTTP responses for snippets.
+
+# Replace with site protocol, domain, and (optionally) port.
+SITE_URL = 'http://localhost:8000'
