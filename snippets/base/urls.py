a7d460f205f5ad0746f325e651b30e22f6c6760e
Kristján Oddsson <koddsson@gmail.com>
2014-11-05 11:54:00 -0800

    Add JSON snippets index view.

--- snippets/base/urls.py
+++ snippets/base/urls.py
@@ -3,18 +3,20 @@ from django.conf.urls.defaults import patterns, url
 from snippets.base import views
 
 
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='base.index'),
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.SnippetIndexView.as_view(), name='base.index'),
     url(r'^(?P<startpage_version>[^/]+)/(?P<name>[^/]+)/(?P<version>[^/]+)/'
         '(?P<appbuildid>[^/]+)/(?P<build_target>[^/]+)/(?P<locale>[^/]+)/'
         '(?P<channel>[^/]+)/(?P<os_version>[^/]+)/(?P<distribution>[^/]+)/'
         '(?P<distribution_version>[^/]+)/$', views.fetch_snippets,
         name='base.fetch_snippets'),
-    url(r'^json/(?P<startpage_version>[^/]+)/(?P<name>[^/]+)/(?P<version>[^/]+)/'
-        '(?P<appbuildid>[^/]+)/(?P<build_target>[^/]+)/(?P<locale>[^/]+)/'
-        '(?P<channel>[^/]+)/(?P<os_version>[^/]+)/(?P<distribution>[^/]+)/'
-        '(?P<distribution_version>[^/]+)/$', views.fetch_json_snippets,
-        name='base.fetch_json_snippets'),
+    url(r'^json/(?P<startpage_version>[^/]+)/(?P<name>[^/]+)/'
+        '(?P<version>[^/]+)/(?P<appbuildid>[^/]+)/(?P<build_target>[^/]+)/'
+        '(?P<locale>[^/]+)/(?P<channel>[^/]+)/(?P<os_version>[^/]+)/'
+        '(?P<distribution>[^/]+)/(?P<distribution_version>[^/]+)/$',
+        views.fetch_json_snippets, name='base.fetch_json_snippets'),
     url(r'^preview/$', views.preview_snippet, name='base.preview'),
     url(r'^show/(?P<snippet_id>\d+)/$', views.show_snippet, name='base.show'),
+    url(r'^json-snippets/', views.JSONSnippetIndexView.as_view(), name='base.index_json'),
 )
