084d9a88b4d589f85afc46fb26910e73dde0a877
Giorgos Logiotatidis <seadog@sealabs.net>
2016-02-16 14:36:01 +0200

    Order CMRs by description.

--- snippets/base/models.py
+++ snippets/base/models.py
@@ -281,7 +281,7 @@ class ClientMatchRule(CachingMixin, models.Model):
     cached_objects = ClientMatchRuleManager()
 
     class Meta:
-        ordering = ('-modified',)
+        ordering = ('description',)
 
     def matches(self, client):
         """Evaluate whether this rule matches the given client."""
