803ad91b2b8851486b3d1f995f89f6ea0fc0b40e
Giorgos Logiotatidis <seadog@sealabs.net>
2016-03-07 19:15:42 +0200

    Add configuration for metrics.

--- snippets/settings.py
+++ snippets/settings.py
@@ -157,8 +157,8 @@ ANON_ALWAYS = True
 
 SNIPPET_BUNDLE_TIMEOUT = 15 * 60  # 15 minutes
 
-METRICS_URL = 'https://snippets-stats.mozilla.org/foo.html'
-METRICS_SAMPLE_RATE = 0.1
+METRICS_URL = config('METRICS_URL', default='https://snippets-stats.mozilla.org/foo.html')
+METRICS_SAMPLE_RATE = config('METRICS_SAMPLE_RATE', default=0.1, cast=float)
 
 GEO_URL = 'https://geo.mozilla.org/country.js'
 
