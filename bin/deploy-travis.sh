620c9b8b477e62874b555e311da744ec2b3ed8b3
Giorgos Logiotatidis <seadog@sealabs.net>
2016-08-22 15:15:44 +0300

    Store deis install script in repo.
    Deis install script is only available through http therefore this commit
    moves it into the repo.
    
    Deis client downloaded by the install script is available through https.

--- bin/deploy-travis.sh
+++ bin/deploy-travis.sh
@@ -11,7 +11,7 @@ docker tag -f ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} ${DOCKER_REPOSITORY}:last_su
 docker push ${DOCKER_REPOSITORY}:last_successful_build
 
 # Install deis client
-curl -sSL http://deis.io/deis-cli/install.sh | sh
+./bin/deis-cli-install.sh
 ./deis login $DEIS_CONTROLLER --username $DEIS_USERNAME --password $DEIS_PASSWORD
 ./deis pull ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} -a $DEIS_APP
 curl -H "x-api-key:$NEWRELIC_API_KEY" \
