6735136d214cb8c73f8d16b24b973d182286147c
Giorgos Logiotatidis <seadog@sealabs.net>
2017-09-28 15:54:44 +0300

    Custom WSGI request_class for manual HTTPS setting.

--- snippets/wsgi/app.py
+++ snippets/wsgi/app.py
@@ -9,14 +9,27 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snippets.settings')  # NOQA
 
+from django.core.handlers.wsgi import WSGIRequest
 from django.core.wsgi import get_wsgi_application
 
 import newrelic.agent
 from decouple import config
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
-application = get_wsgi_application()
 
+IS_HTTPS = os.environ.get('HTTPS', '').strip() == 'on'
+
+
+class WSGIHTTPSRequest(WSGIRequest):
+    def _get_scheme(self):
+        if IS_HTTPS:
+            return 'https'
+
+        return super(WSGIHTTPSRequest, self)._get_scheme()
+
+
+application = get_wsgi_application()
+application.request_class = WSGIHTTPSRequest
 application = Sentry(application)
 
 # Add NewRelic
