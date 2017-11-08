add118c718cbfd824928ebccb515b4c6dab79b3f
Dave Hunt <dave.hunt@gmail.com>
2017-11-08 14:07:31 +0000

    Adding coverage for Activity Stream snippets (#318)
    * Allow 204 as a valid status code, and add coverage for Activity Stream snippets
    
    * Show local variables on test failure rather than including them in assertion messages
    
    * Remove test_all_links because it there hasn't been anchor elements in the payload for ~2 years
    
    * Remove superfluous test class
    
    * Add ids for version parameters
    
    * Run tests in parallel by default
    
    * Verify certificates during tests

--- tests/test_snippets.py
+++ tests/test_snippets.py
@@ -4,7 +4,6 @@
 
 import json
 import re
-from urlparse import urlparse
 from xml.dom.minidom import parseString
 from xml.parsers.expat import ExpatError
 
@@ -14,80 +13,63 @@ import requests
 
 
 REQUESTS_TIMEOUT = 20
+URL_TEMPLATE = '{}/{}/Firefox/default/default/default/en-US/{}/default/default/default/'
+
+_user_agent_firefox = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:10.0.1) Gecko/20100101 Firefox/10.0.1'
 
 
-class TestSnippets:
+def _get_redirect(url, user_agent=_user_agent_firefox, locale='en-US'):
 
-    test_data = [
-        ('/3/Firefox/default/default/default/en-US/release/default/default/default/'),
-        ('/3/Firefox/default/default/default/en-US/aurora/default/default/default/'),
-        ('/3/Firefox/default/default/default/en-US/beta/default/default/default/')]
+    headers = {'user-agent': user_agent,
+               'accept-language': locale}
 
-    _user_agent_firefox = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:10.0.1) Gecko/20100101 Firefox/10.0.1'
+    return requests.get(url, headers=headers, timeout=REQUESTS_TIMEOUT)
 
-    def _get_redirect(self, url, user_agent=_user_agent_firefox, locale='en-US'):
 
-        headers = {'user-agent': user_agent,
-                   'accept-language': locale}
+def _parse_response(content):
+    return BeautifulSoup(content, 'html.parser')
 
-        # verify=False ignores invalid certificate
-        return requests.get(url, headers=headers, verify=False, timeout=REQUESTS_TIMEOUT)
 
-    def assert_valid_url(self, url, path, user_agent=_user_agent_firefox, locale='en-US'):
-        """Checks if a URL returns a 200 OK response."""
+@pytest.mark.parametrize(('version'), ['3', '5'], ids=['legacy', 'activitystream'])
+@pytest.mark.parametrize(('channel'), ['aurora', 'beta', 'release'])
+def test_response_codes(base_url, version, channel):
+    url = URL_TEMPLATE.format(base_url, version, channel)
+    r = _get_redirect(url)
+    assert r.status_code in (requests.codes.ok, requests.codes.no_content)
 
-        # Only check http and https links
-        if not urlparse(url).scheme.startswith('http'):
-            return True
 
-        headers = {'user-agent': user_agent,
-                   'accept-language': locale}
+@pytest.mark.parametrize(('channel'), [
+    'aurora',
+    'beta',
+    pytest.param('release', marks=pytest.mark.xfail(
+        'mozilla' in pytest.config.getoption('base_url'),
+        reason='Activity Stream will be available after Firefox 57'))])
+def test_activity_stream(base_url, channel):
+    url = URL_TEMPLATE.format(base_url, '5', channel)
+    soup = _parse_response(_get_redirect(url).content)
+    script = soup.find('script', type='application/javascript').text
+    snippet_json_string = re.search('JSON\.parse\("(.+)"\)', script).groups()[0]
+    snippet_set = json.loads(snippet_json_string.replace('%u', r'\u').decode('unicode-escape'))
+    assert isinstance(snippet_set, list), 'No snippet set found'
 
-        # HEAD doesn't return page body.
-        try:
-            r = requests.get(url, headers=headers, timeout=REQUESTS_TIMEOUT,
-                             allow_redirects=True, verify=False)
-        except requests.exceptions.RequestException as e:
-            raise AssertionError('Error connecting to {0} in {1}: {2}'.format(
-                url, path, e))
-        assert requests.codes.ok == r.status_code, \
-            'Bad URL {0} found in {1}'.format(url, path)
-        return True
 
-    def _parse_response(self, content):
-        return BeautifulSoup(content)
+def test_legacy(base_url):
+    url = URL_TEMPLATE.format(base_url, '3', 'release')
+    soup = _parse_response(_get_redirect(url).content)
+    script = soup.find('script', type='text/javascript').text
+    snippet_json_string = re.search("JSON\.parse\('(.+)'\)", script).groups()[0]
+    snippet_set = json.loads(snippet_json_string.replace('%u', r'\u').decode('unicode-escape'))
+    assert isinstance(snippet_set, list), 'No snippet set found'
 
-    @pytest.mark.parametrize(('path'), test_data)
-    def test_snippet_set_present(self, base_url, path):
-        full_url = base_url + path
 
-        r = self._get_redirect(full_url)
-        assert requests.codes.ok == r.status_code, full_url
+@pytest.mark.parametrize(('channel'), ['aurora', 'beta', 'release'])
+def test_that_snippets_are_well_formed_xml(base_url, channel):
+    url = URL_TEMPLATE.format(base_url, '3', channel)
 
-        soup = self._parse_response(r.content)
-        snippet_script = soup.find('script', type="text/javascript").text
-        snippet_json_string = re.search("JSON.parse\('(.+)'\)", snippet_script).groups()[0]
-        snippet_set = json.loads(snippet_json_string.replace('%u', r'\u').decode('unicode-escape'))
-
-        assert isinstance(snippet_set, list), 'No snippet set found'
-
-    @pytest.mark.parametrize(('path'), test_data)
-    def test_all_links(self, base_url, path):
-        full_url = base_url + path
-
-        soup = self._parse_response(self._get_redirect(full_url).content)
-        snippet_links = soup.select("a")
-
-        for link in snippet_links:
-            self.assert_valid_url(link['href'], path)
-
-    @pytest.mark.parametrize(('path'), test_data)
-    def test_that_snippets_are_well_formed_xml(self, base_url, path):
-        full_url = base_url + path
-
-        r = self._get_redirect(full_url)
-        try:
-            parseString('<div>{}</div>'.format(r.content))
-        except ExpatError as e:
-            raise AssertionError('Snippets at {0} do not contain well formed '
-                                 'xml: {1}'.format(full_url, e))
+    r = _get_redirect(url)
+    try:
+        print(r.content)
+        parseString('<div>{}</div>'.format(r.content))
+    except ExpatError as e:
+        raise AssertionError('Snippets at {0} do not contain well formed '
+                             'xml: {1}'.format(url, e))
