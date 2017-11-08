bd53b280478aedbcee6fe41542e567ddd905ddd2
Dave Hunt <dave.hunt@gmail.com>
2017-11-08 13:46:54 +0000

    Remove superfluous test class

--- tests/test_snippets.py
+++ tests/test_snippets.py
@@ -13,62 +13,64 @@ import requests
 
 
 REQUESTS_TIMEOUT = 20
+URL_TEMPLATE = '{}/{}/Firefox/default/default/default/en-US/{}/default/default/default/'
+
+_user_agent_firefox = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:10.0.1) Gecko/20100101 Firefox/10.0.1'
 
 
-class TestSnippets:
+def _get_redirect(url, user_agent=_user_agent_firefox, locale='en-US'):
 
-    URL_TEMPLATE = '{}/{}/Firefox/default/default/default/en-US/{}/default/default/default/'
+    headers = {'user-agent': user_agent,
+               'accept-language': locale}
 
-    _user_agent_firefox = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:10.0.1) Gecko/20100101 Firefox/10.0.1'
+    # verify=False ignores invalid certificate
+    return requests.get(url, headers=headers, verify=False, timeout=REQUESTS_TIMEOUT)
 
-    def _get_redirect(self, url, user_agent=_user_agent_firefox, locale='en-US'):
 
-        headers = {'user-agent': user_agent,
-                   'accept-language': locale}
+def _parse_response(content):
+    return BeautifulSoup(content, 'html.parser')
 
-        # verify=False ignores invalid certificate
-        return requests.get(url, headers=headers, verify=False, timeout=REQUESTS_TIMEOUT)
 
-    def _parse_response(self, content):
-        return BeautifulSoup(content, 'html.parser')
+@pytest.mark.parametrize(('version'), ['3', '5'])
+@pytest.mark.parametrize(('channel'), ['aurora', 'beta', 'release'])
+def test_response_codes(base_url, version, channel):
+    url = URL_TEMPLATE.format(base_url, version, channel)
+    r = _get_redirect(url)
+    assert r.status_code in (requests.codes.ok, requests.codes.no_content)
 
-    @pytest.mark.parametrize(('version'), ['3', '5'])
-    @pytest.mark.parametrize(('channel'), ['aurora', 'beta', 'release'])
-    def test_response_codes(self, base_url, version, channel):
-        url = self.URL_TEMPLATE.format(base_url, version, channel)
-        r = self._get_redirect(url)
-        assert r.status_code in (requests.codes.ok, requests.codes.no_content)
 
-    @pytest.mark.parametrize(('channel'), [
-        'aurora',
-        'beta',
-        pytest.param('release', marks=pytest.mark.xfail(
-            'mozilla' in pytest.config.getoption('base_url'),
-            reason='Activity Stream will be available after Firefox 57'))])
-    def test_activity_stream(self, base_url, channel):
-        url = self.URL_TEMPLATE.format(base_url, '5', channel)
-        soup = self._parse_response(self._get_redirect(url).content)
-        script = soup.find('script', type='application/javascript').text
-        snippet_json_string = re.search('JSON\.parse\("(.+)"\)', script).groups()[0]
-        snippet_set = json.loads(snippet_json_string.replace('%u', r'\u').decode('unicode-escape'))
-        assert isinstance(snippet_set, list), 'No snippet set found'
+@pytest.mark.parametrize(('channel'), [
+    'aurora',
+    'beta',
+    pytest.param('release', marks=pytest.mark.xfail(
+        'mozilla' in pytest.config.getoption('base_url'),
+        reason='Activity Stream will be available after Firefox 57'))])
+def test_activity_stream(base_url, channel):
+    url = URL_TEMPLATE.format(base_url, '5', channel)
+    soup = _parse_response(_get_redirect(url).content)
+    script = soup.find('script', type='application/javascript').text
+    snippet_json_string = re.search('JSON\.parse\("(.+)"\)', script).groups()[0]
+    snippet_set = json.loads(snippet_json_string.replace('%u', r'\u').decode('unicode-escape'))
+    assert isinstance(snippet_set, list), 'No snippet set found'
 
-    def test_legacy(self, base_url):
-        url = self.URL_TEMPLATE.format(base_url, '3', 'release')
-        soup = self._parse_response(self._get_redirect(url).content)
-        script = soup.find('script', type='text/javascript').text
-        snippet_json_string = re.search("JSON\.parse\('(.+)'\)", script).groups()[0]
-        snippet_set = json.loads(snippet_json_string.replace('%u', r'\u').decode('unicode-escape'))
-        assert isinstance(snippet_set, list), 'No snippet set found'
 
-    @pytest.mark.parametrize(('channel'), ['aurora', 'beta', 'release'])
-    def test_that_snippets_are_well_formed_xml(self, base_url, channel):
-        url = self.URL_TEMPLATE.format(base_url, '3', channel)
+def test_legacy(base_url):
+    url = URL_TEMPLATE.format(base_url, '3', 'release')
+    soup = _parse_response(_get_redirect(url).content)
+    script = soup.find('script', type='text/javascript').text
+    snippet_json_string = re.search("JSON\.parse\('(.+)'\)", script).groups()[0]
+    snippet_set = json.loads(snippet_json_string.replace('%u', r'\u').decode('unicode-escape'))
+    assert isinstance(snippet_set, list), 'No snippet set found'
 
-        r = self._get_redirect(url)
-        try:
-            print(r.content)
-            parseString('<div>{}</div>'.format(r.content))
-        except ExpatError as e:
-            raise AssertionError('Snippets at {0} do not contain well formed '
-                                 'xml: {1}'.format(url, e))
+
+@pytest.mark.parametrize(('channel'), ['aurora', 'beta', 'release'])
+def test_that_snippets_are_well_formed_xml(base_url, channel):
+    url = URL_TEMPLATE.format(base_url, '3', channel)
+
+    r = _get_redirect(url)
+    try:
+        print(r.content)
+        parseString('<div>{}</div>'.format(r.content))
+    except ExpatError as e:
+        raise AssertionError('Snippets at {0} do not contain well formed '
+                             'xml: {1}'.format(url, e))
