4fb451bcffb2218b0cde6c4ced4bc82b6a711fa9
Giorgos Logiotatidis <seadog@sealabs.net>
2018-01-05 10:51:50 +0200

    Update acceptance tests.

--- bin/acceptance-tests.sh
+++ bin/acceptance-tests.sh
@@ -24,6 +24,18 @@ function check_http_code {
     fi
 }
 
+function check_zero_content_length {
+    echo -n "Checking zero content length of URL ${1} "
+    test=$(curl -L -s ${1} | wc -c);
+    if [[ $test -eq 0 ]];
+    then
+        echo "OK"
+    else
+        echo "Failed"
+        EXIT=1
+    fi
+}
+
 for url in ${URLS[*]}
 do
     check_http_code ${BASE_URL}${url}
@@ -32,8 +44,7 @@ done
 # Check a page that throws 404. Not ideal but will surface 500s
 check_http_code ${BASE_URL}/foo 404
 
-# Check empty bundles
-check_http_code ${BASE_URL}/4/Firefox/56.0.1/20160922113459/WINNT_x86-msvc/xx/release/Windows_NT%206.1/default/default/ 204
-check_http_code ${BASE_URL}/5/Firefox/56.0.1/20160922113459/WINNT_x86-msvc/xx/release/Windows_NT%206.1/default/default/ 204
+check_zero_content_length ${BASE_URL}/4/Firefox/56.0.1/20160922113459/WINNT_x86-msvc/xx/release/Windows_NT%206.1/default/default/
+check_zero_content_length ${BASE_URL}/5/Firefox/56.0.1/20160922113459/WINNT_x86-msvc/xx/release/Windows_NT%206.1/default/default/
 
 exit ${EXIT}
