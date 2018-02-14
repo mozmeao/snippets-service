11f9327a7b6d07f0cacb5eb45e5ce5d3d3979e37
Giorgos Logiotatidis <glogiotatidis@users.noreply.github.com>
2018-02-14 01:57:03 -0600

    Only log blocks in preview mode. (#355)

--- snippets/base/templates/base/includes/snippet_as.js
+++ snippets/base/templates/base/includes/snippet_as.js
@@ -505,6 +505,12 @@ function popFromBlockList(snippetID) {
 }
 
 function addToBlockList(snippetID) {
+    {# In preview mode just log blocking #}
+    {% if preview %}
+    console.log("[preview mode] Blocked snippet: " + snippetID);
+    return;
+    {% else %}
+
     var blockList = getBlockList();
     if (snippetID === undefined) {
         snippetID = ABOUTHOME_SHOWN_SNIPPET.campaign || ABOUTHOME_SHOWN_SNIPPET.id;
