1a932eac3c738adc6e89997ede3c14809404c1d8
Giorgos Logiotatidis <glogiotatidis@users.noreply.github.com>
2018-02-22 03:53:14 -0600

    Add django-allow-cidr (#361)
    * Exclude /healthz from secure redirect.
    
    * Add django-allow-cidr to fix ELB health checks

--- snippets/settings.py
+++ snippets/settings.py
@@ -32,6 +32,7 @@ DEBUG = config('DEBUG', cast=bool)
 DEBUG_TEMPLATE = config('DEBUG_TEMPLATE', default=DEBUG, cast=bool),
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_CIDR_NETS = config('ALLOWED_CIDR_NETS', default='', cast=Csv())
 
 # Application definition
 
@@ -67,6 +68,7 @@ for app in config('EXTRA_APPS', default='', cast=Csv()):
     INSTALLED_APPS.append(app)
 
 MIDDLEWARE_CLASSES = (
+    'allow_cidr.middleware.AllowCIDRMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django_statsd.middleware.GraphiteRequestTimingMiddleware',
     'django_statsd.middleware.GraphiteMiddleware',
@@ -159,6 +161,9 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = False
 SECURE_BROWSER_XSS_FILTER = config('SECURE_BROWSER_XSS_FILTER', default=False, cast=bool)
 SECURE_CONTENT_TYPE_NOSNIFF = config('SECURE_CONTENT_TYPE_NOSNIFF', default=False, cast=bool)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_REDIRECT_EXEMPT = [
+    r'^healthz/$',
+]
 
 TEMPLATES = [
     {
