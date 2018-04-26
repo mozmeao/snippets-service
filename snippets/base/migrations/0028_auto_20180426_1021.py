59d7d358e0724ff04e69270b79b1c3778e33f982
Giorgos Logiotatidis <seadog@sealabs.net>
2018-04-26 08:32:04 +0300

    Fix migration conflict.

--- snippets/base/migrations/0028_auto_20180426_1021.py
+++ snippets/base/migrations/0028_auto_20180426_1021.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0026_snippettemplatevariable_order'),
+        ('base', '0027_auto_20180425_1041'),
     ]
 
     operations = [
