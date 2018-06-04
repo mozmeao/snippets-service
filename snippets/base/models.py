ab04ea5fb0c6f6fccc47586e7618af5e227cdc53
Giorgos Logiotatidis <seadog@sealabs.net>
2018-06-04 20:08:37 +0300

    [Issue #503] Add on_startpage_6 field.

--- snippets/base/models.py
+++ snippets/base/models.py
@@ -425,6 +425,7 @@ class Snippet(SnippetBaseModel):
     on_startpage_3 = models.BooleanField(default=False, verbose_name='Version 3')
     on_startpage_4 = models.BooleanField(default=False, verbose_name='Version 4')
     on_startpage_5 = models.BooleanField(default=True, verbose_name='Activity Stream')
+    on_startpage_6 = models.BooleanField(default=False, verbose_name='Activity Stream NG')
 
     weight = models.IntegerField(
         'Prevalence', choices=SNIPPET_WEIGHTS, default=100,
