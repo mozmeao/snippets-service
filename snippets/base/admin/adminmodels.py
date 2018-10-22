1a4a6350941f7eaa958eb199dff3f0e854b1d408
Giorgos Logiotatidis <seadog@sealabs.net>
2018-10-22 12:45:32 +0300

    Fix obj.creator check.

--- snippets/base/admin/adminmodels.py
+++ snippets/base/admin/adminmodels.py
@@ -186,7 +186,7 @@ class ASRSnippetAdmin(admin.ModelAdmin):
         )
 
     def save_model(self, request, obj, form, change):
-        if not obj.creator:
+        if not obj.creator_id:
             obj.creator = request.user
         statsd.incr('save.asrsnippet')
         super().save_model(request, obj, form, change)
@@ -218,7 +218,7 @@ class CampaignAdmin(admin.ModelAdmin):
     )
 
     def save_model(self, request, obj, form, change):
-        if not obj.creator:
+        if not obj.creator_id:
             obj.creator = request.user
         statsd.incr('save.campaign')
         super().save_model(request, obj, form, change)
@@ -258,7 +258,7 @@ class TargetAdmin(admin.ModelAdmin):
     )
 
     def save_model(self, request, obj, form, change):
-        if not obj.creator:
+        if not obj.creator_id:
             obj.creator = request.user
         statsd.incr('save.target')
         super().save_model(request, obj, form, change)
