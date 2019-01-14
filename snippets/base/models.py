4c87bdf10b47687fe0806b66bc15987a4c6c91af
Giorgos Logiotatidis <seadog@sealabs.net>
2019-01-14 13:29:06 +0200

    [Fix #896] Remove migration code.

--- snippets/base/models.py
+++ snippets/base/models.py
@@ -205,7 +205,6 @@ class SnippetBaseModel(django_mysql.models.Model):
         snippet_copy.creator = creator
         snippet_copy.published = False
         snippet_copy.ready_for_review = False
-        snippet_copy.migrated_to = None
         snippet_copy.uuid = uuid.uuid4()
         snippet_copy.name = '{0} - {1}'.format(
             self.name,
@@ -275,11 +274,6 @@ class Snippet(SnippetBaseModel):
                    'Will be used for snippet blocking if set.'))
 
     ready_for_review = models.BooleanField(default=False)
-
-    migrated_to = models.OneToOneField(
-        'ASRSnippet', null=True, blank=True,
-        related_name='migrated_from',
-        on_delete=models.SET_NULL)
 
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.PROTECT)
     created = models.DateTimeField(auto_now_add=True)
@@ -727,7 +721,6 @@ class ASRSnippet(django_mysql.models.Model):
         snippet_copy.status = STATUS_CHOICES['Draft']
         snippet_copy.creator = creator
         snippet_copy.uuid = uuid.uuid4()
-        snippet_copy.migrated_from = None
         snippet_copy.name = '{0} - {1}'.format(
             self.name,
             datetime.strftime(datetime.now(), '%Y.%m.%d %H:%M:%S'))
