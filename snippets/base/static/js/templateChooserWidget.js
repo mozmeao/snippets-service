623b9508b89f243f7f0652d01cf18f79c7604405
Giorgos Logiotatidis <seadog@sealabs.net>
2019-03-13 11:26:47 +0200

    Fix template chooser on save error.

--- snippets/base/static/js/templateChooserWidget.js
+++ snippets/base/static/js/templateChooserWidget.js
@@ -1,14 +1,23 @@
 ;$(function() {
     'use strict';
 
+    function showTemplate() {
+        let value = $('#id_template_chooser').val();
+        if (value) {
+            $('.inline-template').hide();
+            $('.' + value).show();
+        }
+    }
+
     if ($('.inline-template').length > 1) {
         $('.inline-template').hide();
+
+        // Show correct template on load
+        showTemplate();
+
+        // Show correct template on change
         $('#id_template_chooser').change(function() {
-            let template = $(this).val();
-            $('.inline-template').hide();
-            if (template) {
-                $inline = $('.' + template).show();
-            }
+            showTemplate();
         });
     }
 });
