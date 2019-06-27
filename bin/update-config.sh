a1eadee4d198d5581f6a0627c46df242c3cdab8a
Josh Mize <jgmize@gmail.com>
2019-06-27 13:23:23 -0500

    Update clock-deploy.yaml from bin/update-config.sh

--- bin/update-config.sh
+++ bin/update-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-# env vars: CLUSTER_NAME, CONFIG_BRANCH, CONFIG_REPO, NAMESPACE, DEPLOYMENT_YAML
+# env vars: CLUSTER_NAME, CONFIG_BRANCH, CONFIG_REPO, NAMESPACE
 
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $BIN_DIR/set_git_env_vars.sh # sets DOCKER_IMAGE_TAG
@@ -10,8 +10,10 @@ git clone --depth=1 -b ${CONFIG_BRANCH:=master} ${CONFIG_REPO:=github-mozmar-rob
 cd snippets-config
 
 set -u
-sed -i -e "s|image: .*|image: ${DOCKER_IMAGE_TAG}|" ${CLUSTER_NAME:=oregon-b}/${NAMESPACE:=snippets-dev}/${DEPLOYMENT_YAML:=deploy.yaml}
-git add ${CLUSTER_NAME}/${NAMESPACE}/${DEPLOYMENT_YAML}
+for DEPLOYMENT_YAML in deploy.yaml clock-deploy.yaml; do
+    sed -i -e "s|image: .*|image: ${DOCKER_IMAGE_TAG}|" ${CLUSTER_NAME:=oregon-b}/${NAMESPACE:=snippets-dev}/${DEPLOYMENT_YAML}
+    git add ${CLUSTER_NAME}/${NAMESPACE}/${DEPLOYMENT_YAML}
+done
 git commit -m "set image to ${DOCKER_IMAGE_TAG} in ${CLUSTER_NAME}" || echo "nothing new to commit"
 git push
 popd
