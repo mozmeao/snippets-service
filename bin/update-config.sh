5caa44f9aa63fcbf3368116efca4afcf93b6516d
Josh Mize <jgmize@gmail.com>
2019-07-01 12:19:57 -0500

    Remove bin dir from git add acceptance-tests.sh in update-config.sh

--- bin/update-config.sh
+++ bin/update-config.sh
@@ -18,7 +18,7 @@ for DEPLOYMENT in {admin-,clock-,}deploy.yaml; do
     fi
 done
 cp ${BIN_DIR}/acceptance-tests.sh .
-git add bin/acceptance-tests.sh
+git add acceptance-tests.sh
 git commit -m "set image to ${DOCKER_IMAGE_TAG} in ${CLUSTER_NAME}" || echo "nothing new to commit"
 git push
 popd
