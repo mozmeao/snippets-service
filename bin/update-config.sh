283065b4d4137bbe1713a82715df8a7362944ee7
Giorgos Logiotatidis <seadog@sealabs.net>
2019-07-04 11:53:01 +0300

    Fix typo.

--- bin/update-config.sh
+++ bin/update-config.sh
@@ -22,6 +22,6 @@ done
 
 cp ${BIN_DIR}/acceptance-tests.sh .
 git add acceptance-tests.sh
-git commit -m "set image to ${DOCKER_IMAGE_TAG} in ${CLUSTER_NAME}" || echo "nothing new to commit"
+git commit -m "set image to ${DOCKER_IMAGE_TAG}" || echo "nothing new to commit"
 git push
 popd
