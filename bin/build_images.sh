8420a08047804efff6e2e83481fce621363fc372
Giorgos Logiotatidis <seadog@sealabs.net>
2019-07-16 13:24:29 +0300

    Move from bash scripts to .gitlab-ci.yaml

--- bin/build_images.sh
+++ bin/build_images.sh
@@ -2,9 +2,6 @@
 
 set -exo pipefail
 
-BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $BIN_DIR/set_git_env_vars.sh # sets DOCKER_IMAGE_TAG
-
 function imageExists() {
     docker history -q "${DOCKER_IMAGE_TAG}" > /dev/null 2>&1
     return $?
--- bin/push2dockerhub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $BIN_DIR/set_git_env_vars.sh # sets DOCKER_IMAGE_TAG
-
-# Push to docker hub
-docker push $DOCKER_IMAGE_TAG
