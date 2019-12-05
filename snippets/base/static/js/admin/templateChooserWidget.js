ec3a0f7b12dbc939e4cb62f23c371e50678b0e8b
Giorgos Logiotatidis <seadog@sealabs.net>
2019-12-05 13:47:18 +0200

    [Fix #1253] Show populated inline Template to users with view-only perms.

--- snippets/base/static/js/admin/templateChooserWidget.js
+++ snippets/base/static/js/admin/templateChooserWidget.js
@@ -6,12 +6,20 @@ document.addEventListener(
     'DOMContentLoaded',
     function() {
         function showTemplate() {
-            let value = django.jQuery('#id_template_chooser').val();
             django.jQuery('.inline-template').hide();
+
+            let value = django.jQuery('#id_template_chooser').val();
             if (value) {
                 django.jQuery('.' + value).show();
+                autoTranslate();
             }
-            autoTranslate();
+            // Template Chooser value is empty in two cases: a. When no template
+            // has been selected yet and b. when user has view-only permissions.
+            // In the later case there's a populated inline template which can
+            // be matched with the query bellow.
+            else {
+                django.jQuery('.inline-related.has_original').parent('.inline-template').show();
+            }
         }
 
         // Show correct template on load
