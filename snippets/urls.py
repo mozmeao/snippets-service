cbfad75c6eb1bb19cd5e8d1f740c4b62dc07515b
Giorgos Logiotatidis <seadog@sealabs.net>
2020-02-25 23:25:01 +0200

    [Fix #1186] Rate limit public job list view.

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,15 +1,29 @@
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 from django.views.generic import RedirectView
 from django.views.static import serve as static_serve
+from django.shortcuts import render
 from django.urls import include, path, re_path
+
+import sentry_sdk
+from ratelimit.exceptions import Ratelimited
 
 
 def robots_txt(request):
     permission = 'Allow' if settings.ENGAGE_ROBOTS else 'Disallow'
     return HttpResponse('User-agent: *\n{0}: /'.format(permission), content_type='text/plain')
+
+
+def handler403(request, exception=None):
+    if isinstance(exception, Ratelimited):
+        with sentry_sdk.configure_scope() as scope:
+            scope.level = 'info'
+            scope.set_tag('logger', 'ratelimited')
+            sentry_sdk.capture_message(message='Rate limited')
+        return render(request, template_name='base/ratelimited.jinja', status=429)
+    return HttpResponseForbidden('Forbidden')
 
 
 urlpatterns = [
