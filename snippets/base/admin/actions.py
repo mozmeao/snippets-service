c231dbb80aa2d84dbea93886719aa1fd1d9c7069
Giorgos Logiotatidis <seadog@sealabs.net>
2020-03-23 12:58:08 +0200

    [Fix #1204] Add CSV Export action to metrics related adminmodels.

--- snippets/base/admin/actions.py
+++ snippets/base/admin/actions.py
@@ -1,4 +1,8 @@
+import csv
+from datetime import datetime
+
 from django.db import transaction
+from django.http import HttpResponse
 
 
 @transaction.atomic
@@ -6,3 +10,21 @@ def duplicate_snippets_action(modeladmin, request, queryset):
     for snippet in queryset:
         snippet.duplicate(request.user)
 duplicate_snippets_action.short_description = 'Duplicate selected snippets'  # noqa
+
+
+def export_as_csv(modeladmin, request, queryset):
+    """Adapted from https://books.agiliq.com/projects/django-admin-cookbook/en/latest/export.html"""
+    meta = modeladmin.model._meta
+    field_names = [field.name for field in meta.fields]
+    filename = f'{meta}-{datetime.today().strftime("%Y-%m-%d-%H-%M")}'
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(filename)
+    writer = csv.writer(response)
+
+    writer.writerow(field_names)
+    for obj in queryset:
+        writer.writerow([getattr(obj, field) for field in field_names])
+
+    return response
+export_as_csv.short_description = 'Export Selected to CSV'  # noqa
